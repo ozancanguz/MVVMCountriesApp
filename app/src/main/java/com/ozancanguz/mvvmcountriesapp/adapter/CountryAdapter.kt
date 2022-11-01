011612827259048bd5db792dff57e2cc071e09fc
Ozan <ozancangu24@gmail.com>
2022-11-01 13:47:52 +0300

    -Country adapter coded

---
+++ app/src/main/java/com/ozancanguz/mvvmcountriesapp/adapter/CountryAdapter.kt
@@ -0,0 +1,42 @@
+package com.ozancanguz.mvvmcountriesapp.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.ozancanguz.mvvmcountriesapp.R
+import com.ozancanguz.mvvmcountriesapp.model.Country
+import kotlinx.android.synthetic.main.fragment_country.view.*
+import kotlinx.android.synthetic.main.item_country.view.*
+
+class CountryAdapter(private val countryList:ArrayList<Country>):
+    RecyclerView.Adapter<CountryAdapter.CountryViewHolder>() {
+
+    fun updateCountryList(newCountryList:List<Country>){
+        countryList.clear()
+        countryList.addAll(newCountryList)
+        notifyDataSetChanged()
+    }
+
+
+    class CountryViewHolder(view: View):RecyclerView.ViewHolder(view)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CountryViewHolder {
+        val inflater=LayoutInflater.from(parent.context)
+        val view=inflater.inflate(R.layout.item_country,parent,false)
+        return CountryViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: CountryViewHolder, position: Int) {
+        holder.itemView.name.text=countryList[position].countryName
+        holder.itemView.region.text=countryList[position].countryRegion
+
+
+    }
+
+    override fun getItemCount(): Int {
+        return countryList.size
+    }
+
+
+}
