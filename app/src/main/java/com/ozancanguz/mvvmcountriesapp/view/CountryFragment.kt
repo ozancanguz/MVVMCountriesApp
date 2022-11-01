f595eb367b12bf362b633765a38327e069f3cecd
Ozan <ozancangu24@gmail.com>
2022-11-01 13:12:54 +0300

    -navigation edited

--- app/src/main/java/com/ozancanguz/mvvmcountriesapp/view/CountryFragment.kt
+++ app/src/main/java/com/ozancanguz/mvvmcountriesapp/view/CountryFragment.kt
@@ -11,7 +11,7 @@ import com.ozancanguz.mvvmcountriesapp.R
 
 class CountryFragment : Fragment() {
 
-
+            private var countryUuid=0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -23,6 +23,14 @@ class CountryFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_country, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        arguments?.let {
+            countryUuid=CountryFragmentArgs.fromBundle(it).countryUuid
+        }
     }
 
 
