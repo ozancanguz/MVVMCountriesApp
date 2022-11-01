81eb8cffa03db8cd395d018663b00c6ff8c2e2c4
Ozan <ozancangu24@gmail.com>
2022-11-01 13:02:03 +0300

    -navigation built

---
+++ app/src/main/java/com/ozancanguz/mvvmcountriesapp/view/CountryFragment.kt
@@ -0,0 +1,29 @@
+package com.ozancanguz.mvvmcountriesapp.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ozancanguz.mvvmcountriesapp.R
+
+
+
+class CountryFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_country, container, false)
+    }
+
+
+}
