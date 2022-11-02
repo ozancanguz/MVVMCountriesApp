d3d3cafb7acf92dac268015ec58b4ecd5b6c1901
Ozan <ozancangu24@gmail.com>
2022-11-02 09:58:41 +0300

    -model class edited for room database

--- app/src/main/java/com/ozancanguz/mvvmcountriesapp/model/Country.kt
+++ app/src/main/java/com/ozancanguz/mvvmcountriesapp/model/Country.kt
@@ -1,19 +1,41 @@
 package com.ozancanguz.mvvmcountriesapp.model
 
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
+@Entity
 data class Country(
+
+    @ColumnInfo(name="capital")
     @SerializedName("capital")
     val countryCapital: String,
+
+    @ColumnInfo(name="currency")
     @SerializedName("currency")
     val countryCurrency: String,
+
+
+    @ColumnInfo(name="flag")
     @SerializedName("flag")
     val imageUrl: String,
+
+    @ColumnInfo(name="language")
     @SerializedName("language")
     val countryLanguage: String,
+
+
+    @ColumnInfo(name="name")
     @SerializedName("name")
     val countryName: String,
+
+    @ColumnInfo(name="region")
     @SerializedName("region")
     val countryRegion: String
-)
+){
+
+@PrimaryKey(autoGenerate = true)
+    var uuid=0
+}
